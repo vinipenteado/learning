6a97e3a9aff72e083f765d42065132c8d2cd4c18
Vinicius Penteado Maruyama <vinipenteado@gmail.com>
2025-11-05 11:42:54 +0100

    feat: añadimos un campo computado

--- learning/models/student.py
+++ learning/models/student.py
@@ -7,5 +7,14 @@ class Student(models.Model):
     name = fields.Char(string='Name', required=True)
     vat = fields.Char(string='VAT', required=True)
     birth_date = fields.Date(string='Date of Birth')
-    age = fields.Integer(string='Age')
+    age = fields.Integer(string='Age', compute='_compute_age', store=True)
+
+    @api.depends('birth_date')
+    def _compute_age(self):
+        for record in self:
+            if record.birth_date:
+                today = fields.Date.today()
+                record.age = today.year - record.birth_date.year - ((today.month, today.day) < (record.birth_date.month, record.birth_date.day))
+            else:
+                record.age = 0
     
